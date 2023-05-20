49b2b25443716a9e35ea376cc4bb1b6803b41b5e
Fluturecode <fluturecode@gmail.com>
2023-05-20 14:59:13 -0400

    modified entrypoint

--- src/entrypoint.rs
+++ src/entrypoint.rs
@@ -1,15 +1,22 @@
-use solana_program::{
-    account_info::AccountInfo, entrypoint, entrypoint::ProgramResult, pubkey::Pubkey,
-};
-
 use crate::processor::Processor;
+use solana_program::{account_info::AccountInfo, entrypoint::ProgramResult, msg, pubkey::Pubkey};
 
+#[cfg(not(feature = "no-entrypoint"))]
+use solana_program::entrypoint;
+#[cfg(not(feature = "no-entrypoint"))]
 entrypoint!(process_instruction);
 
-pub fn process_instruction(
+fn process_instruction(
     program_id: &Pubkey,
-    accounts: &[AccountInfo],
-    instruction_data: &[u8],
+    accounts: &[AccountInfo], // Data to read/write
+    instruction_data: &[u8],  // Parameter Input
 ) -> ProgramResult {
-    Processor::process(program_id, accounts, instruction_data)
+    msg!(
+        "process_instruction: {}: {} accounts, data={:?}",
+        program_id,
+        accounts.len(),
+        instruction_data
+    );
+
+    Processor::process_instruction(program_id, accounts, instruction_data)
 }
