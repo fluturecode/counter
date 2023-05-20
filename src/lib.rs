74e8d0f9a118d7857b39400ad668edb64b982ff4
Fluturecode <fluturecode@gmail.com>
2023-05-20 14:59:38 -0400

    modified lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,5 @@
+pub mod entrypoint;
 pub mod error;
 pub mod instruction;
 pub mod processor;
 pub mod state;
-
-#[cfg(not(feature = "no-entrypoint"))]
-pub mod entrypoint;
-
-pub const COUNTER_SEED: &str = "counter";
-pub const SETTINGS_SEED: &str = "settings";
-solana_program::declare_id!("9onZvMzqAFzSHJrLNVWfqLRFFQ5ZCGzNXB4PBxmp6z5Y");
